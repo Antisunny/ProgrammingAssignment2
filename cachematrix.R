35f4f11a0a8c04308927eebb9d5719e485333c6b
Nicole <antisunny@outlook.com>
2014-10-25 16:24:07 +0800

    p.assignment.2 finished
    easy one

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mat <- NULL
+    if(det(x) == 0){ # singular matrix
+        message('Singular, no Inversion')
+        return(NULL)
+    }
+    set <- function(newmat){
+        x <<- newmat
+        mat <<- NULL
+    }
+    get <- function() x
+    setInversion <- function(inver) mat <- inver
+    getInversion <- function() mat
+    list(get = get, set = set, getInver = getInversion,setInver = setInversion)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInver()
+    if(!is.null(m)){
+        # inversion not calculated or
+        # is a singular
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setInver(m)
+    m;x$getInver()
 }
